dc6300fa97133d959876487235df3957506747be
George Kelly <george.kelly@ukfast.co.uk>
2022-03-23 14:48:25 +0000

    Added tests for service provider and search macro

--- tests/Mocks/Pet.php
+++ tests/Mocks/Pet.php
@@ -3,11 +3,20 @@
 namespace Tests\Mocks;
 
 use Illuminate\Database\Eloquent\Model;
+use UKFast\Sieve\Searchable;
 
-class Pet extends Model
+class Pet extends Model implements Searchable
 {
     public function owner()
     {
         return $this->belongsTo(Owner::class);
+    }
+
+    public function sieve($sieve)
+    {
+        return $sieve->configure(fn ($filter) => [
+            'id' => $filter->numeric(),
+            'name' => $filter->string(),
+        ]);
     }
 }
